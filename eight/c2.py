dc76e8ec66e88a46402373748a52e3f7faf2899b
Zhao-666 <2268699370@qq.com>
2018-08-01 08:28:46 +0800

    1

---
+++ eight/c2.py
@@ -0,0 +1,3 @@
+
+def funcname(param):
+    pass
---
+++ eight/c3.py
@@ -0,0 +1,9 @@
+def damage(a1, a2):
+    da1 = a1 * 3
+    da2 = a2 + 5
+    return da1, da2
+
+
+damages1, damages2 = damage(2, 3)
+
+print(damages1,damages2)
